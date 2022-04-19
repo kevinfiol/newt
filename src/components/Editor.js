875f73bca8caae3918c830616f9d5d1ae88a11e0
kevinfiol <me@kevinfiol.com>
2022-04-19 15:02:05 -0400

    correct CodeMirror paths

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,9 @@
 import m from 'mithril';
-import CodeMirror from 'CodeMirror';
-import markdown from 'CodeMirror/mode/markdown/markdown';
-import css from 'CodeMirror/mode/css/css';
-import comment from 'CodeMirror/addon/comment/comment';
-import keymap from 'CodeMirror/keymap/sublime';
+import CodeMirror from 'codemirror';
+import markdown from 'codemirror/mode/markdown/markdown';
+import css from 'codemirror/mode/css/css';
+import comment from 'codemirror/addon/comment/comment';
+import keymap from 'codemirror/keymap/sublime';
 
 export const Editor = ({ attrs: { editorContent, syntax, onInput, lineNumbers } }) => {
     let cm;
